8f78f7502a58b16e3ffcb2a82c44a94acaa8fd38
carlosgodoyberrios <carlos.godoy@simplit-solutions.com>
2021-02-11 13:27:01 -0300

    added first domain and config base

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv';
+import { object } from 'prop-types';
+
+dotenv.config()
+
+export default object.freeze({
+    baseUrl: process.env.BASE_URL  || 'http://localhost('
+})
---
+++ src/infraestructure/api/index.js
@@ -0,0 +1,8 @@
+const MakeApiAdapter = client => ({
+    get: async (resource) => {
+        const { data } = await client.get(resource)
+        return data
+    }
+})
+
+export default MakeApiAdapter
