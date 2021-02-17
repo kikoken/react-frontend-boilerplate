063a9fd6f8a5d608b55b0afe74cac435b043d08a
carlosgodoyberrios <carlos.godoy@simplit-solutions.com>
2021-02-17 10:32:02 -0300

    add module auth domain saga, features and jsconfig path

---
+++ src/domain/auth/sagas/signIn.js
@@ -0,0 +1,13 @@
+import { call, put } from 'redux-saga/effects';
+
+//actions
+import { actionTypes as authActions} from '../features'
+
+export default function* tryAuthSignIn() {
+    try {
+        const data = {}
+        yield put({type: authactions.AUTH_SIGN_IN, data})
+    } catch (error) {
+        yield put({type: authActions.AUTH_FAILED, message: error.message})
+    }
+}
